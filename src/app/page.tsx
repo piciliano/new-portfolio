4803677e356844b7349a6ffb0e6abcf520aad103
neto <picilianovasconcelos@gmail.com>
2025-06-19 16:59:12 -0300

    feat: add responsive footer with social links and gradient background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/app/components/Header";
 import HomeSection from "./components/HomeSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <HomeSection />
+      <Footer />
     </>
   );
 }
